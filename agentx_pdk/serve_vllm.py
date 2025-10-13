f757462441721d5c6f62b45fe1d242dad748fa27
Abhilaksh-sharma-xcal <abhilaksh.sharma@xcaliber.health>
2025-10-13 14:56:19 +0530

    device set: default cuda

--- agentx_pdk/serve_vllm.py
+++ agentx_pdk/serve_vllm.py
@@ -2,6 +2,7 @@ import ray
 from ray import serve
 from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.async_llm_engine import AsyncLLMEngine
+from vllm import SamplingParams
 from typing import Dict, Any
 import os
 import json
@@ -12,11 +13,11 @@ class VLLMModelServer:
     def __init__(self, model_name: str, **kwargs):
         token = os.environ.get("HUGGINGFACE_HUB_TOKEN")
         if token:
-            os.environ["HF_TOKEN"] = token
+            os.environ["HF_TOKEN"] = token  
             print("[VLLM] Using Hugging Face token from env")
 
 
-        engine_args = AsyncEngineArgs(model=model_name, **kwargs)
+        engine_args = AsyncEngineArgs(model=model_name,device=device, **kwargs)
         print(f"[VLLM] Initializing model '{model_name}' with args: {kwargs}")
 
         # Initialize the vLLM async engine
@@ -25,17 +26,19 @@ class VLLMModelServer:
 
     async def generate(self, prompt: str, sampling_params: Dict[str, Any] = None) -> str:
 
-        sampling_params = sampling_params or {
+        params_dict = sampling_params or {
             "temperature": 0.7,
             "top_p": 0.9,
             "max_tokens": 256,
         }
+        
+        sampling_params_obj = SamplingParams(**params_dict)
 
         try:
             request_id = f"req-{self.request_counter}"
             self.request_counter += 1
 
-            results_generator = self.engine.generate(prompt, sampling_params, request_id=request_id)
+            results_generator = self.engine.generate(prompt, sampling_params_obj, request_id=request_id)
             final_output = None
 
             async for request_output in results_generator:
